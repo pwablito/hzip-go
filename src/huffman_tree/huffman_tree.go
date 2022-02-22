4df78d60329edf21ecf39b7a6c8919ba0a576e1a
Spencer, Paul Whiting <pwablito@gmail.com>
2022-02-21 19:25:46 -0500

    Add lookup function for decompression

--- src/huffman_tree/huffman_tree.go
+++ src/huffman_tree/huffman_tree.go
@@ -1,8 +1,45 @@
 package huffman_tree
 
+import (
+	"bytes"
+	"errors"
+
+	"github.com/dgryski/go-bitstream"
+)
+
 type HuffmanTree struct {
 	Head      HTreeNode
 	Frequency int
+}
+
+func (tree *HuffmanTree) Lookup(buf *bytes.Buffer, length int) (byte, bool, error) {
+	/*
+		If found, return (data, true, nil)
+		If not overflow but not target leaf, return (0, false, nil)
+		If overflow return (0, false, error)
+	*/
+	reader := bitstream.NewReader(buf)
+	var current_node HTreeNode = tree.Head
+	for i := 0; i < length; i++ {
+		if current_node.IsLeaf() {
+			return 0, false, errors.New("[ERROR] Tree overflow")
+		}
+		bit, err := reader.ReadBit()
+		if err != nil {
+			return 0, false, errors.New("[ERROR] Buffer overflow")
+		}
+		if bit == bitstream.One { // right
+			current_node = *current_node.(TreeNode).LeftChild
+		} else if bit == bitstream.Zero {
+			current_node = *current_node.(TreeNode).RightChild
+		} else { // left
+			return 0, false, errors.New("[ERROR] Invalid bit")
+		}
+	}
+	if !current_node.IsLeaf() {
+		return 0, false, nil
+	}
+	return current_node.Data(), true, nil
 }
 
 type HTreeNode interface {
